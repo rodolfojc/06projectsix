98e40f3899c93cec2a7dbfee0bc149736d7e374b
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-19 22:27:55 +0100

    Delete
    Demo

--- website/articles/urls.py
+++ website/articles/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('article/<int:pk>', views.ArticleDetailsView.as_view(), name = 'article_page'),
     path('article/new', views.ArticleCreateView.as_view(), name = 'new_article'),
     path('article/<int:pk>/edit', views.ArticleUpdateView.as_view(), name = 'article_edit'),
+    path('article/<int:pk>/delete', views.ArticleDeleteView.as_view(), name = 'article_delete'),
 
 ]
--- website/articles/views.py
+++ website/articles/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 from . models import Article
@@ -22,3 +23,9 @@ class ArticleUpdateView(UpdateView):
     model = Article
     template_name = 'article_edit.html'
     fields = ['title', 'text']
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    context_object_name = 'batman'
+    success_url = reverse_lazy('home')
